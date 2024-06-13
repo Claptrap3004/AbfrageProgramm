10becf02c985b44f433cc011e21c5304137f4d05
Claptrap3004 <r.Kraatz@edu.bbq.de>
2024-06-13 15:32:20 +0200

    created enumeration KindOfIdText.php and class IdText.php

---
+++ code/class/IdText.php
@@ -0,0 +1,48 @@
+<?php
+
+class IdText
+{
+    private int $id;
+    private string $identifier;
+    private KindOfIdText $kindOf;
+
+    /**
+     * @param int $id
+     * @param string $identifier
+     * @param KindOfIdText $kindOf
+     */
+    public function __construct(int $id, string $identifier, KindOfIdText $kindOf)
+    {
+        $this->id = $id;
+        $this->identifier = $identifier;
+        $this->kindOf = $kindOf;
+    }
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function getKindOf(): KindOfIdText
+    {
+        return $this->kindOf;
+    }
+
+
+}
---
+++ code/class/KindOfIdText.php
@@ -0,0 +1,27 @@
+<?php
+
+enum KindOfIdText : string
+{
+    case CATEGORY = 'category';
+    case ANSWER = 'answer';
+
+    case QUESTION = 'question';
+    case STATS = 'stats';
+    case USER = 'user';
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return $this->value;
+    }
+}
