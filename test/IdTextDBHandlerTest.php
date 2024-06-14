60beb91b1906c130d91f72e82de6ad1bb8b02f4d
Claptrap3004 <r.Kraatz@edu.bbq.de>
2024-06-14 11:59:39 +0200

    implemented IdTextDBHandlerTest.php and successfully tested create method

---
+++ test/IdTextDBHandlerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace quiz;
+
+use PHPUnit\Framework\TestCase;
+
+include '../code/class/CanHandleDB.php';
+include '../code/class/MariaDBConnector.php';
+include '../code/class/IdTextDBHandler.php';
+include '../code/class/KindOfIdText.php';
+include_once '../code/class/CanConnectDB.php';
+
+class IdTextDBHandlerTest extends TestCase
+{
+    private CanConnectDB $db;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->db = new MariaDBConnector();
+    }
+
+    public function testCreate()
+    {
+        $arrange = new IdTextDBHandler(KindOfIdText::CATEGORY,$this->db);
+        $act = $arrange->create(['text' => 'newCat']);
+        $this->assertEquals(7, $act);
+
+    }
+}
