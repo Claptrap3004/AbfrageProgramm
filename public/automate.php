45dfad52d2ab2afac7f4866696cb17910b9119af
Claptrap3004 <r.Kraatz@edu.bbq.de>
2024-06-25 23:01:08 +0200

    implemented answerQuestion.html.twig

--- public/automate.php
+++ public/automate.php
@@ -1,9 +1,9 @@
 <?php
-spl_autoload_register(function ($class){
-    $part = explode('\\', $class);
-    $class = $part[1];
-    require '../App/class/' . $class . '.php';
-});
+//spl_autoload_register(function ($class){
+//    $part = explode('\\', $class);
+//    $class = $part[1];
+//    require '../App/class/' . $class . '.php';
+//});
 
 
 spl_autoload_register(function ($class){
--- public/index.php
+++ public/index.php
@@ -3,26 +3,38 @@ require '../vendor/autoload.php';
 require 'automate.php';
 //include '../core\App/class/MariaDBConnector.php';
 use quiz\MariaDBConnector;
-
 $conn = new MariaDBConnector();
 
 $loader = new \Twig\Loader\FilesystemLoader('../App/view');
 $twig = new \Twig\Environment($loader);
-$viewname = "quiz/selectQuestions";
+$viewname = "quiz/answerQuestion";
+//$viewname = "quiz/selectQuestions";
 
-$viewFile = '../App/view/' .  ucfirst($viewname). '.html.twig';
-//$viewFile = "../view/selectQuestions.html.twig";
+//$viewFile = '../App/view/' .  ucfirst($viewname). '.html.twig';
+$viewFile = "../App/view/". $viewname . ".html.twig";
+//$viewFile = "../App/view/quiz/selectQuestions.html.twig";
 if (file_exists($viewFile)) {
-    echo $twig->render("$viewname.html.twig",['categories' => [
-        ['id'=> 1,'name' => 'PC','number'=> 10],
-        ['id'=> 2,'name' => 'Bla','number'=> 15],
-        ['id'=> 3,'name' => 'suelz','number'=> 17],
-        ['id'=> 4,'name' => 'blub','number'=> 19],
-        ['id'=> 4,'name' => 'blub','number'=> 19],
-        ['id'=> 4,'name' => 'blub','number'=> 19],
-        ['id'=> 4,'name' => 'blub','number'=> 19],
-        ['id'=> 5,'name' => 'PC','number'=> 21]
-        ]]);
+
+    echo $twig->render("$viewname.html.twig",['question' => ['id' => 1, 'text'=>'Frage blablblablablbalbl',
+    'answers' => [['id' => 1, 'text' => 'CPU'],
+                ['id' => 2, 'text' => 'RAM'],
+                ['id' => 3, 'text' => 'ROM'],
+                ['id' => 4, 'text' => 'Blub']],
+    'stats' => ['id'=>1, 'timesAsked' => 2, 'timesRight'=> 1]]]);
+
+
+//    echo $twig->render("$viewname.html.twig",['categories' => [
+//        ['id'=> 1,'name' => 'PC','number'=> 10],
+//        ['id'=> 2,'name' => 'Bla','number'=> 15],
+//        ['id'=> 3,'name' => 'suelz','number'=> 17],
+//        ['id'=> 4,'name' => 'blub','number'=> 19],
+//        ['id'=> 4,'name' => 'blub','number'=> 19],
+//        ['id'=> 4,'name' => 'blub','number'=> 19],
+//        ['id'=> 4,'name' => 'blub','number'=> 19],
+//        ['id'=> 5,'name' => 'PC','number'=> 21]
+//        ]]);
+
+
 //            require $viewFile;
 }
 //include "../view/quiz/selectQuestions.html.twig";
