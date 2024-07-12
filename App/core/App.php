d516ad5cf7f61be3f62369f486e6b4fb35e40e46
Claptrap3004 <r.Kraatz@edu.bbq.de>
2024-07-12 14:39:18 +0200

    excluded call of not public methods in Controller

--- App/core/App.php
+++ App/core/App.php
@@ -23,7 +23,13 @@ class App
             $controller1 = $this->prefix . ucfirst($url[0]) . 'Controller';
             $controller = new $controller1;
 
-            if (!empty($url[1])) $this->method = method_exists($controller, $url[1]) ? $url[1] : 'index';
+            if (!empty($url[1])) {
+                if (method_exists($controller, $url[1])) try {
+                    $reflexion = new \ReflectionClass($controller);
+                    $this->method = $reflexion->getMethod($url[1])->isPublic() ? $url[1] : 'index';;
+                } catch (\ReflectionException $e) {
+                }
+            }
             $data = isset($url[2]) ? [$url[2]] : [];
             call_user_func_array([$controller, $this->method], $data);
         } else header("refresh:0.01;url='https://abfrageprogramm.ddev.site:8443/QuizQuestion'");;
