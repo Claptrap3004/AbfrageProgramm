d2e94a901007d0f8f3bba2201610734d678da331
Claptrap3004 <r.Kraatz@edu.bbq.de>
2024-08-15 11:22:16 +0200

    tried to arrange items properly

--- public/assets/js/selectQuestionToEdit.js
+++ public/assets/js/selectQuestionToEdit.js
@@ -6,23 +6,83 @@ const initSelectQuestions = () => {
     createRadioButtons();
 }
 
+const createTableDiv = () => {
+
+
+    let div = document.createElement('div');
+    div.className = "row align-self-center mx-2 pb-2";
+
+    let table = document.createElement('table');
+    let row = document.createElement('row');
+    let idTh = document.createElement('th');
+    let textTh = document.createElement('th');
+    let catTh = document.createElement('th');
+    row.className = "bold btn btn-primary rounded-3 shadow w-100 ml-3 mr-5 pr-5";
+
+    idTh.innerHTML = 'ID';
+    textTh.innerHTML = 'Question - text';
+    catTh.innerHTML = 'Category';
+
+    idTh.style.width = '15%';
+    textTh.style.width = '78%';
+    catTh.style.width = '12%';
+
+    row.appendChild(idTh);
+    row.appendChild(textTh);
+    row.appendChild(catTh);
+    table.appendChild(row);
+    table.id = 'questionTable';
+    div.appendChild(table);
+
+    return div;
+}
 const createRadioButtons = () => {
+    document.querySelector('#showQuestions').appendChild(createTableDiv());
+
+
     for (const question of questions) {
         let element = createRadioButton(question)
-        document.querySelector('#showQuestions').appendChild(element);
+        document.querySelector('#questionTable').appendChild(element);
     }
+}
+const createTableRow = (question) => {
+
+    let tableRow = document.createElement('row');
+    let idCol = document.createElement('td');
+    let textCol = document.createElement('td');
+    let catCol = document.createElement('td');
+
+    idCol.id = 'idCol' + question.id;
+    textCol.id = 'textCol' + question.id;
+    catCol.id = 'catCol' + question.id;
+
+    idCol.innerHTML = question.id;
+    textCol.innerHTML = question.text;
+    catCol.innerHTML = question.category;
+
+    idCol.style.width = '10%';
+    textCol.style.width = '75%';
+    catCol.style.width = '15%';
+
+    tableRow.appendChild(idCol);
+    tableRow.appendChild(textCol);
+    tableRow.appendChild(catCol);
+
+    return tableRow
 }
 
 const createRadioButton = (question) => {
+
     let div = document.createElement('div');
     let label = document.createElement('label');
     let input = document.createElement('input');
+
 
     label.setAttribute('data-bs-toggle', 'button');
     label.addEventListener('click', clickQuestion)
 
 
-    div.className = "row align-self-center mx-5 pb-2";
+    div.className = "row align-self-center mx-3 pb-2";
     label.className = "questionLabels col align-self-center bold btn btn-outline-info rounded-3 shadow btn-lg  mx-3 my-2 p-1 w-100";
     input.className = "btn-check";
 
@@ -31,7 +91,7 @@ const createRadioButton = (question) => {
     label.id = 'ql_id' + question.id;
     label.for = 'ql_id' + question.id;
 
-    label.innerHTML = question.text;
+    label.appendChild(createTableRow(question))
     input.type = 'radio';
     input.name = 'questionId';
     input.autocomplete = 'off';
